0338cef8ac7648bb229a44833d94a0705dca7fcf
MassFuer <maxime.ducamp@gmail.com>
2025-09-30 15:52:00 +0200

    Fixing Flake8 errors

--- app/app/test.py
+++ app/app/test.py
@@ -5,11 +5,11 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """
     Test the calc module.
     """
-    
     def test_add_numbers(self):
         """ Test adding numbers together """
         res = calc.add(5, 6)
@@ -18,7 +18,6 @@ class CalcTests(SimpleTestCase):
 
     def test_subtract_numbers(self):
         """ Test subtracting numbers."""
-    
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
